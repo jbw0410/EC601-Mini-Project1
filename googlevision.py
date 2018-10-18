f288c82377ab75a9334c469ed6f9165242723f5e
jbw0410 <35003146+jbw0410@users.noreply.github.com>
2018-10-18 17:43:26 -0400

    Update googlevision.py

--- googlevision.py
+++ googlevision.py
@@ -5,6 +5,11 @@ import os
 from google.cloud import vision
 from google.cloud.vision import types
 
+import PIL
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
 # Instantiates a client
 client = vision.ImageAnnotatorClient()
 for i in range(1,16):
@@ -24,4 +29,24 @@ for i in range(1,16):
 	print('image%dlabels:'%i)
 	for label in labels:
 		print(label.description)
-		print(label.score)
+#		print(label.score)
+
+
+	font=ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 30)
+
+	im1=Image.open(file_name)
+
+
+	draw=ImageDraw.Draw(im1)
+
+	j=0
+
+	for label in labels:
+
+		draw.text((0,j),label.description,(255,255,0),font=font)
+
+		j+=30
+	
+	draw=ImageDraw.Draw(im1)
+
+	im1.save("%d.jpg"%(i))
