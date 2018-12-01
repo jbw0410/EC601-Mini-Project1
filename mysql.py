46f39a180a6100bcd69f39eb8aa63d69ee5ea8cc
jbw0410 <35003146+jbw0410@users.noreply.github.com>
2018-12-01 01:50:06 -0500

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -1,4 +1,20 @@
-import pymysql
+import pymysql  
 
-def ():
-  pass
+db= pymysql.connect(host="localhost",user="debian-sys-maint",
+     password="bu",db="mini3_test",port=3306)
+
+
+cur = db.cursor()
+
+
+sql = "select id from twi_info;"
+try:
+    cur.execute(sql)     
+    results = cur.fetchall()    
+    for i in results:
+        print(i)
+    print('done!')
+except Exception as e:
+    raise e
+finally:
+    db.close()    
